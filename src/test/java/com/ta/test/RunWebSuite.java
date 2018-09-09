e82396ae9779102838560c088b4f771852a60d23
edge <techqaindia@gmail.com>
2018-09-09 11:02:33 +0530

    added json file support

--- src/test/java/com/ta/test/RunWebSuite.java
+++ src/test/java/com/ta/test/RunWebSuite.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/webFeatures",plugin = {"pretty", "com.epam.reportportal.cucumber.ScenarioReporter"},glue = "com.ta.test")
+@CucumberOptions(features = "src/test/resources/webFeatures",plugin = {"pretty", "com.epam.reportportal.cucumber.ScenarioReporter","json:target/cucumber-report/runwebat/cucumber.json",
+        "rerun:target/cucumber-report/runwebat/rerun.txt"},glue = "com.ta.test")
 public class RunWebSuite {
 }
